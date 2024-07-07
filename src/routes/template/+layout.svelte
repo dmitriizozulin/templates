7b8abfad672665cafb1759fde2dc06903c25c742
Dmitrii Zozulin <dmitriizozulin@gmail.com>
2024-07-07 10:43:58 +0000

    Add vercel analytics

--- src/routes/template/+layout.svelte
+++ src/routes/template/+layout.svelte
@@ -3,12 +3,16 @@
     import "@common/styles/main.css";
     import "@common/styles/font.css";
 
+    import { inject } from '@vercel/analytics';
+
     import { introduceDependency } from "@hooks/introduceDependency";
 
     import {
         TemplateRepositoryToken,
         createTemplateRepository,
     } from "@domain/template/template.repository";
+
+    inject();
 
     introduceDependency(TemplateRepositoryToken, createTemplateRepository);
 </script>
